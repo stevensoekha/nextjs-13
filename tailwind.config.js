65fab792358dae1dea7dc531183d90a15397f8e6
Steven Soekha <36703601+stevensoekha@users.noreply.github.com>
2022-11-02 10:47:56 +0100

    Setup basic notes page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-    content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        container: {
+            screens: {
+                sm: '640px',
+                md: '768px',
+                lg: '768px',
+                xl: '768px',
+                '2xl': '768px',
+            },
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Roboto Slab', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
 }
